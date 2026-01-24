4a1c527f529e1fd0d9c681c5d7619f502f02878e
xaxxoo <bnahmad83@gmail.com>
2026-01-24 06:04:18 +0100

    feat: Create LayerZeroModule to encapsulate LayerZeroService and LayerZeroController

---
+++ src/layer-zero/layerzero.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { LayerZeroService } from './services/layerzero.service';
+import { LayerZeroController } from './controllers/layerzero.controller';
+
+@Module({
+  imports: [ConfigModule],
+  controllers: [LayerZeroController],
+  providers: [LayerZeroService],
+  exports: [LayerZeroService],
+})
+export class LayerZeroModule {}
